e830a65d7a52d99cf9a19b866badacc05e752b7e
amir-sabzi <97.amirsabzi@gmail.com>
2020-02-12 16:31:20 +0330

    update 1.01

--- advanced_sniffer.c
+++ advanced_sniffer.c
@@ -29,14 +29,13 @@
 #include <linux/spinlock.h>
 #include <linux/semaphore.h>
 
-static struct semaphore sem;
 
 #define SUCCESS 0
 #define MY_MODULE_NAME "advanced_sniffer"
 #define MAX_SYS_BUF_LEN 32
 #define MAX_PROC_BUF_LEN 4096
 #define LFS_MAGIC 0x19980122
-#define TMPSIZE 20
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Amir Sabzi <97.amirsabzi@gmail.com>");
@@ -52,7 +51,6 @@ static struct inode *lfs_make_inode(struct super_block *sb, int mode)
 	if (ret) {
 		ret->i_mode = mode;
 		ret->i_blocks = 0;
-		// ret->i_fop = &lfs_file_ops;
 		ret->i_atime = ret->i_mtime = ret->i_ctime = current_time(ret);
 	}
 	return ret;
@@ -80,8 +78,7 @@ static ssize_t lfs_read_file(struct file *filp, char *buf,
 
 
 	len = snprintf(tmp, MAX_PROC_BUF_LEN*10000, "%s\n", data);
-	if(FS_buffer_ptr > 0)
-		len = FS_buffer_ptr;
+	len = FS_buffer_ptr;
 	if (*offset > len)
 		return 0;
 	if (count > len - *offset)
@@ -140,37 +137,6 @@ static struct dentry *lfs_create_file (struct super_block *sb,
   out:
 	return 0;
 }
-static struct dentry *lfs_create_dir (struct super_block *sb,
-		struct dentry *parent, const char *name)
-{
-	struct dentry *dentry;
-	struct inode *inode;
-	struct qstr qname;
-
-	qname.name = name;
-	qname.len = strlen (name);
-		//EEEEEEEEEEEEEEEEEEEE
-	qname.hash_len = hashlen_string(parent, name);
-	dentry = d_alloc(parent, &qname);
-	if (! dentry)
-		goto out;
-
-	inode = lfs_make_inode(sb, S_IFDIR | 0644);
-	if (! inode)
-		goto out_dput;
-	inode->i_op = &simple_dir_inode_operations;
-	inode->i_fop = &simple_dir_operations;
-
-	d_add(dentry, inode);
-	return dentry;
-
-  out_dput:
-	dput(dentry);
-  out:
-	return 0;
-}
-
-//static atomic_t counter, subcounter;
 static void lfs_create_files (struct super_block *sb, struct dentry *root)
 {
 	struct dentry *subdir;
@@ -238,8 +204,6 @@ static int lfs_fill_super (struct super_block *sb, void *data, int silent)
   out:
 	return -ENOMEM;
 }
-
-
 /*
  * Stuff to pass in when registering the filesystem.
  */
@@ -251,27 +215,10 @@ static struct dentry *lfs_get_super(struct file_system_type *fst,
 
 static struct file_system_type lfs_type = {
 	.owner 		= THIS_MODULE,
-	.name		= "lwnfs",
+	.name		= "sniffer_fs",
 	.mount		= lfs_get_super,
 	.kill_sb	= kill_litter_super,
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
 
@@ -283,7 +230,6 @@ static struct file_system_type lfs_type = {
 
 //---------------------sysfs interface initial variables  --------------------//
 static char* list_of_interfaces[5] = {"lo\n","enp3s0\n","wlp2s0\n","virbr0\n","virbr0-nic\n"};
-//static char* list_of_ports = {"FTP:20 [0], SSH:22 [1], Telnet:23 [2], smtp:25 [3] , *SSL:443 [4], DNS:53 [5], "HTTP", "other"};
 static struct kobject *our_kobj;
 static char sys_interface_buffer[MAX_SYS_BUF_LEN] = "wlp2s0\n";
 static char sys_port_buffer[MAX_SYS_BUF_LEN],global_interface_name[MAX_SYS_BUF_LEN];
@@ -368,7 +314,6 @@ static struct proc_dir_entry* dstPort_log_file;
 static struct proc_dir_entry* time_log_file;
 static struct proc_dir_entry* sniff_log_file;
 
-static char* protocols[6] = {"UDP","TCP","IP","ICMP","L2TP","OSPF"};
 static int protocol_counter[6] = {0};
 static char GENERAL_BUFFER[1000*4096];
 static int GENERAL_BUFFER_ptr = 0;
@@ -488,7 +433,6 @@ static int proc_saddr_show(struct seq_file *m, void *v){
 	printk(KERN_EMERG "advanced_sniffer: you entered the show function and next lines are log\n");
   seq_printf(m, "|----------------------------------------------------|\n");
   seq_printf(m, "|********************TOP 10 soucre IPs***************|\n");
-  seq_printf(m, "|----------------------------------------------------|\n");
 	seq_printf(m, "|     rank      |       IP address     |   PACKET#   |\n");
   int  top10_IP_index[10];
   get_top10_index(src_addrs_counter,1000,top10_IP_index);
@@ -604,53 +548,11 @@ struct iphdr *ip_header;
 struct udphdr *udp_header;
 struct tcphdr *tcp_header;
 
-static struct file *filp = NULL;
-//static unsigned int file_size = 0;
-static loff_t file_offset = 0;
-static int file_counter = 0;
-//static spinlock_t lock;
-
 //------------------start of sniffer netfilter functions sectoin---------------//
 static void standardize_interface_name(char* buf){
   sprintf(global_interface_name,"%s",buf);
   global_interface_name[strlen(global_interface_name)-1] = 0;
 }
-
-struct file *file_open(const char *path, int flags, int rights)
-{
-    mm_segment_t oldfs;
-    int err = 0;
-
-    oldfs = get_fs();
-    set_fs(get_ds());
-    filp = filp_open(path, flags, rights);
-    set_fs(oldfs);
-    if (IS_ERR(filp)) {
-        err = PTR_ERR(filp);
-        return NULL;
-    }
-    return filp;
-}
-
-void file_close(struct file *file)
-{
-    filp_close(file, NULL);
-}
-
-int file_write(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size)
-{
-    mm_segment_t oldfs;
-    int ret;
-
-    oldfs = get_fs();
-    set_fs(get_ds());
-
-    ret = kernel_write(file, data, size, &offset);
-
-    set_fs(oldfs);
-    return ret;
-}
-
 
 static int sniffer_counter = 0;
 //static int PREVIUOS_BUFFER_ptr = 0;
@@ -674,18 +576,12 @@ unsigned int sniff_hook_func(void *priv, struct sk_buff *skb, const struct nf_ho
 					unsigned char daddr[4];
 					//printk(KERN_INFO "IPADDRESSFILTER: %s packet SRC:(%d.%d.%d.%d) --> DST: (%d.%d.%d.%d)\n", ip_header->protocol == IPPROTO_TCP ? "TCP" : "UDP", saddr[0], saddr[1], saddr[2], saddr[3], daddr[0], daddr[1], daddr[2], daddr[3]);
 					change_address_to_string((unsigned int) ip_header->saddr,saddr);
-					change_address_to_string((unsigned int) ip_header->saddr,daddr);
+					change_address_to_string((unsigned int) ip_header->daddr,daddr);
 					int add = sprintf((char*)(GENERAL_BUFFER + GENERAL_BUFFER_ptr),"\n\n\n%s packet SRC:(%d.%d.%d.%d) --> DST: (%d.%d.%d.%d)\n", ip_header->protocol == IPPROTO_TCP ? "TCP" : "UDP",saddr[0], saddr[1], saddr[2], saddr[3], daddr[0], daddr[1], daddr[2], daddr[3]);
 					GENERAL_BUFFER_ptr = GENERAL_BUFFER_ptr + add;
 
-
-
-
-
 					i = 0;
-					printk(KERN_ALERT "counter %d",sniffer_counter);
 					if(sniffer_counter > 20){
-						printk(KERN_ALERT "counter %d",sniffer_counter);
 						sniffer_counter = 0;
 						while(i < GENERAL_BUFFER_ptr - FS_buffer_ptr){
 							FS_buffer[i +  FS_buffer_ptr] = GENERAL_BUFFER[i +  FS_buffer_ptr];
@@ -929,7 +825,7 @@ static int __init advanced_sniffer_init(void){
   printk(KERN_INFO "advanced_sniffer:File system have been Registered.\n");
 	int return_value ;
 	printk(KERN_INFO "advanced_sniffer: Initialization\n");
-	our_kobj = kobject_create_and_add(MY_MODULE_NAME, NULL);
+	our_kobj = kobject_create_and_add("sniffer", NULL);
 	if (!our_kobj){
 		printk(KERN_ALERT "advanced_sniffer: KOBJECT Registration Failure.\n");
 		return -ENOMEM;
